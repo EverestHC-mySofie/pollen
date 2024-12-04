24884f7136bfa9ca3a5bfa03db14b4ec419812cc
Jef Mathiot <jeff.mathiot@gmail.com>
2024-12-04 19:21:57 +0100

    Fix lint

--- test/dummy/config/initializers/pollen.rb
+++ test/dummy/config/initializers/pollen.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Pollen.common.configure do |c|
-  c.redis Redis.new(url: "redis://127.0.0.1:6379")
+  c.redis Redis.new(url: 'redis://127.0.0.1:6379')
 end
 
 Pollen.server.configure do |c|
-  c.authenticate do |request, env|
+  c.authenticate do |_request, _env|
     User.first
   end
 end
